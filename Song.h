a0ffe81cdc4a8a8e856d92461f6d69fd3ff2d843
karencl <69648059+karencl@users.noreply.github.com>
2021-09-19 15:15:01 -0500

    Datos de canciones y archivo de su estructura

---
+++ Song.h
@@ -0,0 +1,35 @@
+/*
+ * Proyecto MusicApp
+ * Karen Cebreros López
+ * A01704254
+ * 8/09/2021
+ *
+ * song.h
+ * MusicApp
+ * Created by Karen CL on 8/09/21.
+ * Copyright © 2021 KarenCbrs. All rights reserved.
+ */
+
+/*
+ * La estructura Song, tiene simplemente los datos (nombre, autor y duración) de
+ * cada canción y punteros que se utilizan para conectarlas en una lista
+ * doblemente enlazada, ya que es la manera en la que normalmente fucniona una
+ * aplicación de música para recorrer las canciones.
+ */
+
+#ifndef Song_h
+#define Song_h
+
+using namespace std;
+
+// Struct Song
+struct Song {
+    // Pointers and data
+    Song *previous;
+    string name;
+    string author;
+    int duration;
+    Song *next;
+};
+
+#endif /* Song_h */
